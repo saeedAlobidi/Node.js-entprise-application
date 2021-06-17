ec3a804966441502388109d216b885cfdf13dfb6
SAEED ALOBIDI <saeedmohammedsalehalobidi@gmail.com>
2021-06-17 11:42:08 -0700

    PreparedStatement db

--- database/Db.js
+++ database/Db.js
@@ -1,8 +1,13 @@
 module.exports = function makeDb({ sql, config }) {
 
+
+    const QUERY_TYPE = {
+        INSERT: "INSERT",
+        DELETE: "DELETE",
+        UPDATE: "UPDATE",
+        SELECT: "SELECT",
+    }
     async function Query(commands) {
-
-
         let pool = await new sql.ConnectionPool(config).connect();
         let result1 = await pool.request().query(commands);
         await pool.close()
@@ -10,7 +15,20 @@ module.exports = function makeDb({ sql, config }) {
     }
 
 
-    return { Query };
+    async function PreparedStatement(query, values) {
+
+
+        let pool = await new sql.ConnectionPool(config).connect();
+        let request = pool.request();
+        values.forEach((val, index) => { request.input(`v${index}`, sql.VarChar(1000), val) });
+
+        let result1 = await request.query(query);
+        await pool.close()
+
+        return await result1.recordsets[0];
+    }
+
+    return { Query, PreparedStatement, QUERY_TYPE };
 }
 
 
--- domain/repository/User.js
+++ domain/repository/User.js
@@ -1,28 +1,27 @@
 
-module.exports= function makeUserRepository (db) {
-  
- 
-    async function addUser (user) { 
-      
-     
-      //TODO: this function not compeleted
-        return  db.Query("INSERT INTO [dbo].[Users] ([Name] ,[Age] ,[DateCreated])VALUES('"+user.Name+"','"+user.Age+"','"+user.DateCreated+"')");  
+module.exports = function makeUserRepository(db) {
 
-    }  
-    async function updateUser (user) {  
-      
-       //TODO: this function not compeleted
-      return  db.Query("");  
 
-      
-    } 
-    async function getAllUser () {  
-      
-      //TODO: this function not compeleted
-     return  db.Query("select * from [dbo].[Users]");  
+  async function addUser(user) {
 
-     
-   } 
-    return {addUser,updateUser,getAllUser};
+
+    //TODO: this function not compeleted
+    return db.PreparedStatement("INSERT INTO [dbo].[Users] ([Name] ,[Age] ,[DateCreated])VALUES(@v0,@v1,@v2)", [user.Name, user.Age, user.DateCreated]);
 
   }
+  async function updateUser(user) {
+
+    //TODO: this function not compeleted
+    return db.Query("");
+
+
+  }
+  async function getAllUser() {
+
+
+
+    return db.PreparedStatement("select * from users ", ['1'])
+  }
+  return { addUser, updateUser, getAllUser };
+
+}
