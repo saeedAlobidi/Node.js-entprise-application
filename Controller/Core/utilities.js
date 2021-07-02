feacc61697e29c8ce97bb4d6a7afe154723127fb
SAEED ALOBIDI <saeedmohammedsalehalobidi@gmail.com>
2021-07-02 07:54:46 -0700

    files upload dependency

--- Controller/Core/utilities.js
+++ Controller/Core/utilities.js
@@ -11,7 +11,34 @@
 
 
 
+  
+const upload = require("multer")
 module.exports = (() => {
 
- 
+    
+function multerConf() {
+
+    return {
+  
+        storage: upload.diskStorage({
+            destination: function (req, file, cb) {
+  
+              
+                console.log(req.path);
+                cb(null, "./file-upload")
+            },
+            filename: async function (req, file, cb) {
+                const ext = file.mimetype.split('/')[1];
+                let ImageName = Date.now() + "." + ext;
+                UploadName=ImageName;
+            
+                cb(null, ImageName);
+            }
+        })
+  
+    }
+  }
+
+
+ return {multerConf}
 });
--- Controller/user.js
+++ Controller/user.js
@@ -12,15 +12,17 @@
 
 const middleWare = require('../server/MiddleWare')
 const express = require('express')
+const upload = require("multer")
 const constants = require('../constraints')
 const domain = require('../domain')
 const router = express.Router();
 const permission = require('./Core/Permission')
+const controllerUtilities = require('./Core/utilities')(); 
 
 
+router.post('/AddUser', permission.check("AddUser"),upload(controllerUtilities.multerConf()).single('file'), middleWare.task.asyncMiddleHandler(async (req, res) => {
 
-router.post('/AddUser', permission.check("AddUser"), middleWare.task.asyncMiddleHandler(async (req, res) => {
-
+     console.log(req.filecls)
      let data = await domain.useCase.user.addUser(req.body);
 
      res.status(constants.status.SUCCESS).send(data);
